f9583c350b64bdc6168183e26a5a2c031c88c5e4
Nepu9728 <156566298+Closure-2271@users.noreply.github.com>
2025-01-30 22:46:17 +0800

    Web Update

--- Web/scripts/iframe.js
+++ Web/scripts/iframe.js
@@ -1,11 +1,22 @@
-const links = document.querySelectorAll('a.iframe');
-//iframe traget
-const iframe = document.getElementById('avater-iframe');
+document.addEventListener("DOMContentLoaded", function () {
 
-links.forEach(link => {
-    link.addEventListener('click',function(event) {
-        event.preventDefault();
-        const url = this.href;
-        iframe.src = url;
+    const links = document.querySelectorAll("#side-bar ul li a");
+    const iframe = document.createElement("iframe");
+    iframe.id = "content-frame";
+    iframe.style.width = "100%";
+    iframe.style.height = "100vh";
+    iframe.style.position = "absolute";
+    iframe.style.left = "184px";
+    iframe.style.top = "100px";
+    iframe.style.border = "none";
+
+    document.body.appendChild(iframe);
+
+    links.forEach(link => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const url = this.href;
+            iframe.src = url; 
+        });
     });
 });
